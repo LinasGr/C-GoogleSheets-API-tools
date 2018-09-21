18883fe765575ca1ad902dfd8e2dfdbda9c1b56b
LinasGr <linas.grabnickas@gmail.com>
2018-09-21 21:03:50 +0300

    My Google Sheets API tools

---
+++ Debuger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheetTools
+
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var sheet1 = new GoogleSheet("1W126R96CXLJJ7x1R14rh70RtGOEEQnJv5J7E32Jx7wI");
+      var result = sheet1.GetCellData("Balance", "A", "1");
+      Console.WriteLine($"{sheet1.GetCellData("Balance", "B", "1")}+{sheet1.GetCellData("Balance","C","1")}={result}");
+      sheet1.RawUpdate(sheet1.GenerateData(new string[1]{"A1"}), "A1");
+      Console.WriteLine($"{sheet1.GetCellData("Balance", "B", "1")}+{sheet1.GetCellData("Balance", "C", "1")}={result}");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ SheetTools/GoogleSheet.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace SheetTools
+{
+  public class GoogleSheet
+  {
+    string[] Scopes = { SheetsService.Scope.Spreadsheets };
+    string applicationName = "CSharp";
+    String sheetID;
+    SheetsService service;
+    public String range;
+    public ValueRange values;
+
+    public GoogleSheet(string spreadSheetID)
+    {
+      this.sheetID = spreadSheetID;
+      this.service = AuthorizeGoogleApp();
+      this.range = "A1";
+      this.values = new ValueRange();
+      this.values.Values = new List<IList<object>>();
+      this.values.Values.Add(new List<object>());
+    }
+
+    public GoogleSheet()
+    {
+      this.sheetID = "";
+      this.service = AuthorizeGoogleApp();
+      this.range = "A1";
+      this.values = new ValueRange();
+      this.values.Values = new List<IList<object>>();
+      this.values.Values.Add(new List<object>());
+    }
+
+    private SheetsService AuthorizeGoogleApp()
+    {
+      UserCredential credential;
+      using (var stream =
+          new FileStream("client_id.json", FileMode.Open, FileAccess.Read))
+      {
+        string credPath = System.Environment.GetFolderPath(
+            System.Environment.SpecialFolder.Personal);
+        credPath = "token.json";
+        credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+            GoogleClientSecrets.Load(stream).Secrets,
+            this.Scopes,
+            "user",
+            CancellationToken.None,
+            new FileDataStore(credPath, true)).Result;
+        Console.WriteLine("Credential file saved to: " + credPath);
+      }
+      // Create Google Sheets API service.
+      var service = new SheetsService(new BaseClientService.Initializer()
+      {
+        HttpClientInitializer = credential,
+        ApplicationName = applicationName,
+      });
+      return service;
+    }
+
+    public Object GetCellData(string tab, string col, string raw)
+    {
+      this.range = tab + "!" + col + raw;
+      SpreadsheetsResource.ValuesResource.GetRequest request = this.service.Spreadsheets.Values.Get(this.sheetID, this.range);
+      request.ValueRenderOption = SpreadsheetsResource.ValuesResource.GetRequest.ValueRenderOptionEnum.FORMATTEDVALUE;
+      this.values = request.Execute();
+      if (values.Values != null && values.Values.Count > 0)
+        return values.Values[0][0];
+      else return null;
+    }
+
+    public void UpdateCellData(string tab, string col, string raw)
+    {
+      this.range = tab + "!" + col + raw;
+      this.values.Values = GenerateData();
+      SpreadsheetsResource.ValuesResource.UpdateRequest request =
+        service.Spreadsheets.Values.Update(this.values, this.sheetID, this.range);
+      request.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
+      request.Execute();
+    }
+    public void UpdateCellData()
+    {
+      this.values.Values = GenerateData();
+      SpreadsheetsResource.ValuesResource.UpdateRequest request =
+        service.Spreadsheets.Values.Update(this.values, this.sheetID, this.range);
+      request.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
+      request.Execute();
+    }
+    public IList<IList<Object>> GenerateData(string[] arr)
+    {
+      List<IList<Object>> objNewRecords = new List<IList<Object>>();
+
+      IList<Object> obj = new List<Object>();
+      foreach (var item in arr)
+      {
+        obj.Add(item);
+      }
+      objNewRecords.Add(obj);
+
+      return objNewRecords;
+    }
+
+    public void RawAppent(IList<IList<Object>> values, string newRange)
+    {
+      SpreadsheetsResource.ValuesResource.AppendRequest request =
+         this.service.Spreadsheets.Values.Append(new ValueRange() { Values = values }, this.sheetID, newRange);
+      request.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.OVERWRITE;
+      request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+      var response = request.Execute();
+    }
+    public void RawUpdate(IList<IList<object>> values, string newRange)
+    {
+      SpreadsheetsResource.ValuesResource.UpdateRequest request =
+        this.service.Spreadsheets.Values.Update(new ValueRange() { Values = values }, this.sheetID, newRange);
+      request.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
+      var response = request.Execute();
+    }
+  }
+}
